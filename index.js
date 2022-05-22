1a6cb067ec62e0512aeec41011f0fa27cb2b649e
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-05-22 23:35:54 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// displaying the first block of question
+document.getElementsByClassName('container')[0].style.display = "block";
+
+// next function
+function next(id){
+    document.getElementsByClassName('container')[id-1].style.display = "none"; 
+    document.getElementsByClassName('container')[id].style.display = "block"; 
+}
+
+// result function
+function result(){
+    var score = 0;
+    if (document.getElementById('correct1').checked){
+        score++;
+    }
+    if (document.getElementById('correct2').checked){
+        score++;
+    }
+    if (document.getElementById('correct3').checked){
+        score++;
+    }
+    alert("your score is "+ score);
+    
+}
